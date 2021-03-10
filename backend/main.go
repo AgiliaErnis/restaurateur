00ff147ee2980a99b9543e45b419096c875b5e27
Adam Zaiter <zaiter.adam7@gmail.com>
2021-03-10 21:01:40 +0100

    add phone number and website for restaurants

--- backend/main.go
+++ backend/main.go
@@ -13,6 +13,8 @@ func main() {
 	}
 	for _, restaurant := range restaurants {
 		fmt.Println(restaurant.Name)
+		fmt.Println(restaurant.URL)
+		fmt.Println(restaurant.PhoneNumber)
 	}
 	restaurantMenus, err := scraper.GetRestaurantMenus()
 	if err != nil {
@@ -20,6 +22,5 @@ func main() {
 	}
 	for _, menu := range restaurantMenus {
 		fmt.Println(menu.RestaurantName)
-		fmt.Println(menu.WeeklyMenu)
 	}
 }
