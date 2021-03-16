cc901e86e2e93d4a4dcdb45fdb51825fc887417c
Adam Zaiter <zaiter.adam7@gmail.com>
2021-03-16 14:32:49 +0100

    added more tests

--- backend/scraper/scraper_test.go
+++ backend/scraper/scraper_test.go
@@ -3,12 +3,24 @@ package scraper
 import "testing"
 
 func TestSliceContains(t *testing.T) {
-    inputSlice := []string{"test1", "test2", "test3"}
-    inputVal := "test2"
-    expected := true
-    actual := sliceContains(inputSlice, inputVal)
-    if actual != expected {
-        t.Errorf("expected %t for slice %v and value %s, got %t",
-        expected, inputSlice, inputVal, actual)
-    }
+	type sliceContainsTest struct {
+		inputSlice []string
+		inputVal   string
+		expected   bool
+	}
+	var sliceContainsTests = []sliceContainsTest{
+		sliceContainsTest{[]string{"test1", "test2", "test3"}, "test2", true},
+		sliceContainsTest{[]string{}, "test2", false},
+		sliceContainsTest{[]string{}, "", false},
+		sliceContainsTest{[]string{"test1", "test2", "test3"}, "", false},
+		sliceContainsTest{[]string{"test1", "test3"}, "test2", false},
+		sliceContainsTest{[]string{"val"}, "val", true},
+		sliceContainsTest{[]string{"val"}, "val1", false},
+	}
+	for _, test := range sliceContainsTests {
+		if actual := sliceContains(test.inputSlice, test.inputVal); actual != test.expected {
+			t.Errorf("expected %t for slice %v and value %s, got %t",
+				test.expected, test.inputSlice, test.inputVal, actual)
+		}
+	}
 }
