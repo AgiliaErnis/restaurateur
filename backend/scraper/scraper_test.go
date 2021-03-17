bf1cc96b4317926c6608b9769a7481275311a1cb
Adam Zaiter <zaiter.adam7@gmail.com>
2021-03-17 19:47:48 +0100

    added quotes

--- backend/scraper/scraper_test.go
+++ backend/scraper/scraper_test.go
@@ -10,17 +10,17 @@ func TestSliceContains(t *testing.T) {
 	}
 
 	sliceContainsTests := []sliceContainsTest{
-		sliceContainsTest{[]string{"test1", "test2", "test3"}, "test2", true},
-		sliceContainsTest{[]string{}, "test2", false},
-		sliceContainsTest{[]string{}, "", false},
-		sliceContainsTest{[]string{"test1", "test2", "test3"}, "", false},
-		sliceContainsTest{[]string{"test1", "test3"}, "test2", false},
-		sliceContainsTest{[]string{"val"}, "val", true},
-		sliceContainsTest{[]string{"val"}, "val1", false},
+		{[]string{"test1", "test2", "test3"}, "test2", true},
+		{[]string{}, "test2", false},
+		{[]string{}, "", false},
+		{[]string{"test1", "test2", "test3"}, "", false},
+		{[]string{"test1", "test3"}, "test2", false},
+		{[]string{"val"}, "val", true},
+		{[]string{"val"}, "val1", false},
 	}
 	for _, test := range sliceContainsTests {
 		if actual := sliceContains(test.inputSlice, test.inputVal); actual != test.expected {
-			t.Errorf("expected %t for slice %v and value %s, got %t",
+			t.Errorf("expected `%t` for slice %q and value %q, got `%t`",
 				test.expected, test.inputSlice, test.inputVal, actual)
 		}
 	}
