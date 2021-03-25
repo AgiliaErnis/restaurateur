8aa8bf115510ec655cb968adfd59b0dd9a3658d7
Lucius <arthur.s.price@protonmail.com>
2021-03-25 21:27:04 +0100

    begin work on unit tests
    Created Cards.test.js and HeroSection.test.js. Began work on Cards.test.js. Pushed to GitHub on Ani's request.

--- frontend/src/components/cards/CardItem.js
+++ frontend/src/components/cards/CardItem.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import { Link, BrowserRouter as Router } from 'react-router-dom';
 import './Cards.css'
@@ -7,8 +8,39 @@ function CardItem(props) {
     <>
       <Router>
         <li className='cards__item'>
-          <Link className='cards__item__link' to={props.path}>
+          <Link className='cards__item__link'>
             <figure className='cards__item__pic-wrap' data-category={props.label}>
+              <img
+                className='cards__item__img'
+                alt='Food Category Image'
+                src={props.src}
+              />
+            </figure>
+            <div className='cards__item__info'>
+              <h5 className='cards__item__text'>{props.text}</h5>
+            </div>
+          </Link>
+        </li>
+      </Router>
+    </>
+  );
+}
+
+export default CardItem;
+*/
+
+
+import React from 'react';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
+import './Cards.css'
+
+function CardItem(props) {
+  return (
+    <>
+      <Router>
+        <li className='cards__item'>
+          <Link className='cards__item__link' path={props.path}>
+            <figure className='cards__item__pic-wrap' label={props.label}>
               <img
                 className='cards__item__img'
                 alt='Food Category Image'
--- frontend/src/components/cards/Cards.test.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import Cards from "./Cards";
-
-test('testing', () => {
-    const root = document.createElement("div");
-    ReactDOM.render(<Cards />, root);
-    expect(root.querySelector("h1").textContent).toBe("Check Out the Top Suggestions! ");
-})
