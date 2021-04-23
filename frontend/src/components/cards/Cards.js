18cf4d2c4b0d937f50319057af1ef020b0be5cd0
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2021-04-23 22:07:44 +0400

    Updated logo and cards content

--- frontend/src/components/cards/Cards.js
+++ frontend/src/components/cards/Cards.js
@@ -10,35 +10,35 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/Home/breakfast.webp'
-              text='Top breakfast places in Prague'
-              label='Breakfast'
+              src='images/Home/international.webp'
+              text="View the city's more than 1000 International restaurants"
+              label='International'
               path='/restaurants'
             />
             <CardItem
               src='images/Home/italian.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-              label='Italian Food'
+              text="Explore all the Italian restaurants of Prague"
+              label='Italian'
               path='/restaurants'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/Home/coffee.jpg'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-              label='Coffee'
+              src='images/Home/czech.jpg'
+              text='Find your most suitable Czech restaurant'
+              label='Czech'
               path='/restaurants'
             />
             <CardItem
-              src='images/Home/dinner.webp'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-              label='Dinner Places'
+              src='images/Home/glutenfree.webp'
+              text='Check out  the restaurants with gluten free options'
+              label='Gluten Free'
               path='/restaurants'
             />
             <CardItem
-              src='images/Home/salad.webp'
-              text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-              label='Healthy menus'
+              src='images/Home/vegan.jpeg'
+              text='Check out the restaurants with vegan and vegetarian meals'
+              label='Vegan/Vegetarian'
               path='/restaurants'
             />
           </ul>
